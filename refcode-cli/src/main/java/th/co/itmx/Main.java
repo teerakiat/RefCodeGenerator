8de02a2a1cd4dd3033669fff97171f600dcd1b62
ITMX\teerakiat.chi <teerakiat.chi@itmx.co.th>
2018-06-25 06:39:27 +0700

    change to rsa public/pri key encrypt

--- refcode-cli/src/main/java/th/co/itmx/Main.java
+++ refcode-cli/src/main/java/th/co/itmx/Main.java
@@ -9,9 +9,20 @@ import th.co.itmx.util.Util;
 
 import javax.xml.bind.DatatypeConverter;
 import java.io.Console;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import static th.co.itmx.util.Util.byteArrayToHexString;
 
 
 public class Main {
@@ -20,80 +31,125 @@ public class Main {
     //nodejs: Buffer.from('74656572616b696174636869afabacad', 'utf8').toString('hex');
     // 5123456789abcdef1234567890abcdef
     //java: Integer.toHexString(Integer.parseInt(hex));
-    private static String inputKey(String questionText) throws Exception{
-        char secretKey[];
-        String confirm="n";
-        int minimumKeyLength = 32; //having at least 32 Hexadecimal
-        do {
-            System.out.println("Please enter your key in Hexadecimal, supported char [0-9][A-F][a-f], input at least "+minimumKeyLength+" chars");
-            Console console = System.console();
-            secretKey = console.readPassword(questionText);
-            String secretKeyStr = new String(secretKey);
+//    private static String inputKey(String questionText) throws Exception{
+//        char secretKey[];
+//        String confirm="n";
+//        int minimumKeyLength = 32; //having at least 32 Hexadecimal
+//        do {
+//            System.out.println("Please enter your key in Hexadecimal, supported char [0-9][A-F][a-f], input at least "+minimumKeyLength+" chars");
+//            Console console = System.console();
+//            secretKey = console.readPassword(questionText);
+//            String secretKeyStr = new String(secretKey);
+//
+//            if (!secretKeyStr.matches("^[0-9a-fA-F]{"+minimumKeyLength+",}$")) {
+//                System.out.println("invalid key format, please enter your input again");
+//                continue;
+//            }
+//
+//            System.out.println("Confirm following key is correct??: " + secretKeyStr);
+//            System.out.print("[y/n]");
+//            confirm = console.readLine();
+//
+//        }while(!confirm.equalsIgnoreCase("y") );
+//        Util.clearScreen();
+//        return new String(secretKey);
+//    }
 
-            if (!secretKeyStr.matches("^[0-9a-fA-F]{"+minimumKeyLength+",}$")) {
-                System.out.println("invalid key format, please enter your input again");
-                continue;
-            }
-
-            System.out.println("Confirm following key is correct??: " + secretKeyStr);
-            System.out.print("[y/n]");
-            confirm = console.readLine();
-
-        }while(!confirm.equalsIgnoreCase("y") );
-        Util.clearScreen();
-        return new String(secretKey);
+    private static byte[] generateRandomKey () throws Exception{
+        byte[] bytes = new byte[32];
+        SecureRandom.getInstanceStrong().nextBytes(bytes);
+        return bytes;
     }
 
-    private static void generateKey(String outputFile) throws Exception {
+    private static void generateEncryptedKey(String itmx_pub_file, String telco_pub_file, String outputFolder) throws Exception {
         try {
-            if(StringUtils.isEmpty(outputFile)){
-                throw new InvalidFormatException("Output file is mandatory, please provide output file");
-            }
+            KeyFactory kf = KeyFactory.getInstance("RSA");
 
-            //create input bytes, full key compose of key of two components
-            // 1: itmx key
-            // 2: telco key
-            String itmxSecretKey = inputKey("Enter ITMX Secret Key: ");
-            String telcoSecretKey = inputKey("Enter Telco Secret Key: ");
+            byte[] pubKeyBytes = Files.readAllBytes(new File(itmx_pub_file).toPath());
+            X509EncodedKeySpec pubSpec = new X509EncodedKeySpec(pubKeyBytes);
+            PublicKey itmxPub = kf.generatePublic(pubSpec);
 
-            //combine two key and convert to byte array
-            byte[] fullKey = Util.xorWithKey(DatatypeConverter.parseHexBinary(itmxSecretKey), DatatypeConverter.parseHexBinary(telcoSecretKey));
+            pubKeyBytes = Files.readAllBytes(new File(telco_pub_file).toPath());
+            pubSpec = new X509EncodedKeySpec(pubKeyBytes);
+            PublicKey telcoPub = kf.generatePublic(pubSpec);
 
-            System.out.println("full key: "+ Util.byteArrayToHexString(fullKey));
-
+            byte[] shareKey = generateRandomKey();
             //encryption
             KeyManager manager = new KeyManager();
-            byte[] encryptedKey = manager.encrypt(fullKey);
-//            System.out.println("encrypt key: "+ byteArrayToHexString(encryptedKey));
+            byte[] itmxEncryptedKey = manager.encrypt(shareKey, itmxPub);
+            byte[] telcoEncryptedKey = manager.encrypt(shareKey, telcoPub);
+
+            //********** for testing only
+//            System.out.println("encrypt key: "+ byteArrayToHexString(shareKey));
+
+            //create output folder if not exist
+            new File(outputFolder).mkdir();
+
             //write encryption to file
-            try (FileOutputStream fos = new FileOutputStream(outputFile)) {
-                fos.write(encryptedKey);
+            String itmxOutputFile = outputFolder + "\\itmx_key.enc";
+            String telcoOutputFile = outputFolder + "\\telco_key.enc";
+
+            try (FileOutputStream fos = new FileOutputStream(itmxOutputFile)) {
+                fos.write(itmxEncryptedKey);
+                fos.close();
+            }
+            try (FileOutputStream fos = new FileOutputStream(telcoOutputFile)) {
+                fos.write(telcoEncryptedKey);
                 fos.close();
             }
 
-            //decrypt
-//            byte[] resultByte = manager.decrypt(encryptedKey);
+            //********* decrypt, for testing only
+//            System.out.println("encrypted result : " + byteArrayToHexString(itmxEncryptedKey));
+//            byte[] priKeyBytes = Files.readAllBytes(new File("D:\\temp\\itmx_private_key.der").toPath());
+//            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(priKeyBytes);
+//            PrivateKey pri = kf.generatePrivate(spec);
+//
+//            byte[] resultByte = manager.decrypt(itmxEncryptedKey, pri);
 //            System.out.println("result : " + byteArrayToHexString(resultByte));
         }catch (Exception ex){
             System.out.println(ex.getMessage());
         }
     }
 
+    private static void validateNotNull(String value, String varName) throws InvalidFormatException{
+        if (value == null) {
+            throw new InvalidFormatException(varName + " is mandatory");
+        }
+    }
+
     public static void main(String[] args) {
-        String commandList = "genkey, otp";
+        String commandList = "gen, otp";
         Options options = new Options();
 
         Option command = new Option("c", "command", true, "execution command, possible value : "+ commandList);
         command.setRequired(true);
         options.addOption(command);
 
-        Option keyfile = new Option("f", "keyfile", true, "keyfile output in case of genkey, keyfile as input in case of otp");
-        keyfile.setRequired(true);
-        options.addOption(keyfile);
+        //require only for gen command
+        Option option_itmx_pub_key_path = new Option("f", "itmx_pub", true, "itmx public file for encrypted output");
+        option_itmx_pub_key_path.setRequired(false);
+        options.addOption(option_itmx_pub_key_path);
 
-        Option interval = new Option("t", "request_date", true, "date to be test in format yyyyMMdd : 20180621");
-        interval.setRequired(false);
-        options.addOption(interval);
+        Option option_telco_pub_key_path = new Option("t", "telco_pub", true, "telco public file for encrypted output");
+        option_telco_pub_key_path.setRequired(false);
+        options.addOption(option_telco_pub_key_path);
+
+        Option option_output_key_path = new Option("o", "output", true, "output encrypted share key folder path");
+        option_output_key_path.setRequired(false);
+        options.addOption(option_output_key_path);
+
+        //Require only for testing otp
+        Option opton_pri_key_path = new Option("p", "pri_key", true, "private key for testing otp");
+        opton_pri_key_path.setRequired(false);
+        options.addOption(opton_pri_key_path);
+
+        Option option_request_date = new Option("d", "request_date", true, "requested date for testing otp ex. 20180621");
+        option_request_date.setRequired(false);
+        options.addOption(option_request_date);
+
+        Option option_encrypted_key = new Option("e", "encrypted_key", true, "encrypted key path for testing otp");
+        option_encrypted_key.setRequired(false);
+        options.addOption(option_encrypted_key);
 
         HelpFormatter formatter = new HelpFormatter();
 
@@ -104,20 +160,43 @@ public class Main {
 
             String commandValue = cmd.getOptionValue("command");
 
-            if (commandValue.equalsIgnoreCase("genkey")) {
-                String outputFile = cmd.getOptionValue("keyfile");
-                generateKey(outputFile);
-            } else if (commandValue.equalsIgnoreCase("otp")) {
-                String keyFilePath = cmd.getOptionValue("keyfile");
-                String intervalValue = cmd.getOptionValue("request_date");
-                if (intervalValue == null) {
-                    throw new InvalidFormatException("request_date is mandatory");
-                }
+            if (commandValue.equalsIgnoreCase("gen")) {
+                String itmx_pub_file = cmd.getOptionValue("itmx_pub");
+                String telco_pub_file = cmd.getOptionValue("telco_pub");
+                String output_folder = cmd.getOptionValue("output");
 
-                byte[] encryptedSeed = Files.readAllBytes(Paths.get(keyFilePath));
-                byte[] seed = KeyManager.newInstance().decrypt(encryptedSeed);
+                validateNotNull(itmx_pub_file, "itmx public key file");
+                validateNotNull(telco_pub_file, "telco public key file");
+                if(output_folder == null){
+                    Path path = FileSystems.getDefault().getPath(".").toAbsolutePath();
+                    output_folder = path.toString() + "/output";
+                }
+                System.out.println("itmx public key file path: "+itmx_pub_file);
+                System.out.println("telco public key file path: "+telco_pub_file);
+                System.out.println("output folder: "+output_folder);
+
+                generateEncryptedKey(itmx_pub_file, telco_pub_file, output_folder);
+
+            } else if (commandValue.equalsIgnoreCase("otp")) {
+                String private_key_path = cmd.getOptionValue("pri_key");
+                String requested_date = cmd.getOptionValue("request_date");
+                String encrypted_key = cmd.getOptionValue("encrypted_key");
+                validateNotNull(private_key_path, "private key file path");
+                validateNotNull(encrypted_key, "encrypted share secret key file path");
+                validateNotNull(requested_date, "requested date ex. 20180621");
+
+                // Generate Private Key
+                byte[] priKeyBytes = Files.readAllBytes(new File(private_key_path).toPath());
+                KeyFactory kf = KeyFactory.getInstance("RSA");
+                PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(priKeyBytes);
+                PrivateKey pri = kf.generatePrivate(spec);
+
+                // Decrypt seed
+                byte[] encryptedSeed = Files.readAllBytes(Paths.get(encrypted_key));
+                byte[] seed = KeyManager.newInstance().decrypt(encryptedSeed, pri);
+
                 System.out.println("Verifying reference code....");
-                generateRefCode(seed, intervalValue);
+                generateRefCode(seed, requested_date);
 
             }else{
                 System.out.println("Incorrect command code provide, possible command code are: "+commandList);
--- refcodegenerator/src/main/java/th/co/itmx/otp/KeyManager.java
+++ refcodegenerator/src/main/java/th/co/itmx/otp/KeyManager.java
@@ -1,7 +1,18 @@
 package th.co.itmx.otp;
 
+import th.co.itmx.otp.exception.InvalidFormatException;
+
+import javax.crypto.Cipher;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class KeyManager {
     private String key = "YrLmCdNQ4OCZz23f2STHAYZnbwF8pWWd";
+//    private String rsa = "RSA/ECB/OAEPWithSHA1AndMGF1Padding";
+    private String rsa = "RSA/ECB/PKCS1Padding";
 
     public static KeyManager newInstance(){
         return new KeyManager();
@@ -10,23 +21,35 @@ public class KeyManager {
     /*
      *
      */
-    public byte[] encrypt(byte[] plainText) throws Exception{
+    public byte[] encrypt(byte[] plainText) throws Exception {
         byte[] encryptionKey = key.getBytes();
-
-//        System.out.println(encryptionKey.length);
-
+        //        System.out.println(encryptionKey.length);
         AesManager advancedEncryptionStandard = new AesManager(
                 encryptionKey);
         return advancedEncryptionStandard.encrypt(plainText);
     }
 
-    public byte[] decrypt(byte[] encryptKey) throws Exception{
+    public byte[] encrypt(byte[] plainText, PublicKey publicKey) throws Exception{
+        Cipher cipher = Cipher.getInstance(rsa);
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] encryptedMsg = cipher.doFinal(plainText);
+        return  encryptedMsg;
+    }
+
+    public byte[] decrypt(byte[] cipherText) throws Exception{
         byte[] encryptionKey = key.getBytes(
 
         );
         AesManager advancedEncryptionStandard = new AesManager(
                 encryptionKey);
-        return advancedEncryptionStandard.decrypt(encryptKey);
+        return advancedEncryptionStandard.decrypt(cipherText);
     }
 
+    public byte[] decrypt(byte[] cipherText, PrivateKey privateKey) throws Exception{
+
+        Cipher decriptCipher = Cipher.getInstance(rsa);
+        decriptCipher.init(Cipher.DECRYPT_MODE, privateKey);
+
+        return decriptCipher.doFinal(cipherText);
+    }
 }
